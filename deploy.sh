4328389d8909c7589d8fee3241cbe3047d8f769c
wupeng <836720919@qq.com>
2024-02-23 05:56:44 +0800

    deploy at 2024-02-23 05:56

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,9 @@ time=$(date "+%Y-%m-%d %H:%M")
 
 # 初次部署的时候需要这两条命令
 # git init
-git checkout main
+# git checkout main
 git add -A
 git commit -m "deploy at ${time}"
 
 # 部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:gaoxiaosi/canvas-magic.git main:gh-pages
+git push -f git@github.com:gaoxiaosi/canvas-magic.git gh-pages
