17cd5b62ed3f28910c233748c85f46b6f0d4de85
wupeng <836720919@qq.com>
2024-04-09 04:47:05 +0800

    add:新增连连看logo

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 export default [
   { "id": 1, "name": "五子棋", "path": "gobang" },
-  { "id": 2, "name": "2048", "path": "game2048" }
+  { "id": 2, "name": "2048", "path": "game2048" },
+  { "id": 3, "name": "连连看", "path": "link" }
 ]
--- logo.js
+++ logo.js
@@ -1,8 +1,10 @@
 // 批量导入所有的logo
 import gobang from './logo/gobang.png'
 import game2048 from './logo/game2048.png'
+import link from './logo/link.png'
 
 export {
   gobang,
-  game2048
+  game2048,
+  link
 }
