c3e1a8cfa1f2df8ec0798a6929647ae9e48a95d9
wupeng <836720919@qq.com>
2024-04-27 01:10:35 +0800

    perf:优化五子棋算法代码

--- alpha/gobang/index.js
+++ alpha/gobang/index.js
@@ -53,7 +53,7 @@ const drawPiece = (x, y, isBlack) => {
   ctx.fill();
 }
 
-// V1：判断游戏胜负，(x, y)当前下棋坐标，role：黑1白2，chess：棋盘数据，二维数组，黑1白2空位-1
+// 判断游戏胜负，(x, y)当前下棋坐标，role：黑1白2，chess：棋盘数据，二维数组，黑1白2空位-1
 // const isWin = (x, y, role, chess) => {
 //   for (let [dx, dy] of [[1, 0], [0, 1], [1, 1], [1, -1]]) {
 //     let count = 1, i = 0, j = 0;
@@ -64,7 +64,7 @@ const drawPiece = (x, y, isBlack) => {
 //   return false
 // }
 
-// V2：使用some方法再次简化
+// 使用some方法进行简化
 const isWin = (x, y, role, chess) => [[1, 0], [0, 1], [1, 1], [1, -1]].some(([dx, dy]) => {
   let count = 1, i = 0, j = 0;
   while(count < 5 && chess[x + dx * ++i]?.[y + dy * i] === role) count++
